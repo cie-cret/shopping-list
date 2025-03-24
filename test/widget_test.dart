2ec12b4844f8a78d46f5cdf1126549a3b30ba8c6
cie-cret <meplelin@gmail.com>
2025-03-25 01:48:14 +0700

    edit mode fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:a4_shopping_list/main.dart';
@@ -13,7 +14,11 @@ import 'package:a4_shopping_list/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await dotenv.load();
+    final firebaseURL = dotenv.env['FIREBASE_URL'] ?? '';
+    await tester.pumpWidget(MyApp(
+      firebaseURL: firebaseURL,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
